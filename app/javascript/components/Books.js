faa9d4320c6875f73188b16aa62f80ef6417d340
Jedediah Wood <jedediahwood28@gmail.com>
2021-09-06 16:34:27 -0600

    mostly styled, dfn

--- app/javascript/components/Books.js
+++ app/javascript/components/Books.js
@@ -6,15 +6,16 @@ const Books = (props) => {
 
   const renderBooks = () => {
     if (books.length == 0) {
-      return <h1>NO MORE BOOKS FOR THE CHILDREN</h1>
+      return <h1 className='no-mo'>NO MORE BOOKS FOR THE CHILDREN</h1>
     }
     return books.map( book => <Book updateBook={updateBook} deleteBook={deleteBook} key={book.id} {...book}  />)
   }
 
   return(
     <>
-      <div className='books-container'>
-        <h1>Books:</h1>
+      {/* <div className='books-container'> */}
+      <div>
+        <h2 className='books-colon'>Books:</h2>
         {renderBooks()}
       </div>
     </>
